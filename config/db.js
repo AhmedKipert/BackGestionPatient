299ec8545d012db26305adeb55f25fd3841d4df6
AhmedKipert <workahmed@gmail.com>
2025-08-11 17:57:16 +0200

    fix: mongo_uri

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 // Connexion à la base de données
 const connectDB = async() => {
     try {
-        mongoose.connect(process.env.MONGO_URL);
+        mongoose.connect(process.env.MONGO_URI);
         console.log("Connexion réussi à la base de donnée");
     } catch (error) {
         console.log(error);
